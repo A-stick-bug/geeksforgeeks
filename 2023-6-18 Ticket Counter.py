a94294c5e3baa3a6c40830fa17a96c6c54896036
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-06-18 11:49:17 -0400

    first question

---
+++ 2023-6-18 Ticket Counter.py
@@ -0,0 +1,16 @@
+def distributeTicket(N : int, K : int) -> int:
+    nums = range(N)
+    start, end = 0, N-1
+    while end > start:
+        for i in range(K):
+            if end <= start:
+                return nums[start]+1
+            start += 1
+
+        for i in range(K):
+            if end <= start:
+                return nums[start]+1
+            end -= 1
+    return nums[start]+1
+
+print(distributeTicket(5,1))
